0f95fc80c3565ca7798a108e413a2cd2ae1e4633
Harry <poripori1030@gmail.com>
2018-02-15 17:06:27 +0800

    Activity Navigation for BottomNavigationView

--- app/src/main/java/com/devconcept/www/devstagramandroid/SearchActivity.java
+++ app/src/main/java/com/devconcept/www/devstagramandroid/SearchActivity.java
@@ -1,7 +1,8 @@
 package com.devconcept.www.devstagramandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,15 +10,19 @@ import android.view.MenuItem;
 import com.devconcept.www.devstagramandroid.Utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class HomeActivity extends AppCompatActivity {
+/**
+ * Created by harry on 15/02/2018.
+ */
 
-    private static final String TAG = "HomeActivity";
-    private static final int ACTIVITY_NUM = 0;
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+    private static final int ACTIVITY_NUM = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: started");
 
         setupBottomNavigationView();
     }
